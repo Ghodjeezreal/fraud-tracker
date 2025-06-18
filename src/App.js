0ed9056e3f74a572c113f5f8e8ec0f204e54c42c
Fredrick (Ghod) Okojie <160248180+Ghodjeezreal@users.noreply.github.com>
2025-06-18 23:46:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Sidebar from './components/Sidebar';
 import MetricsCards from './components/MetricsCards';
 import TransactionsTable from './components/TransactionsTable';
+import LiveMetricsChart from './components/LiveMetricsChart'; // 👈 import the chart
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Sidebar />
       <main className="flex-1 p-6 bg-gray-100 min-h-screen">
         <MetricsCards />
+        <LiveMetricsChart /> {/* 👈 add chart here */}
         <TransactionsTable />
       </main>
     </div>
