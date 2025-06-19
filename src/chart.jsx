5b219a0d40dc3fc67ae3d35571cd01f77ed0a942
Fredrick (Ghod) Okojie <160248180+Ghodjeezreal@users.noreply.github.com>
2025-06-19 05:59:50 +0100

    Create chart.jsx

---
+++ src/chart.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LiveMetricsChart from './components/LiveMetricsChart';
+
+const root = ReactDOM.createRoot(document.getElementById('chart-container'));
+root.render(<LiveMetricsChart />);
