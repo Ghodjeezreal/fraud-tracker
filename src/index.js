02253f13b49963a2e343001236c02c7cc2b2d14d
Fredrick (Ghod) Okojie <160248180+Ghodjeezreal@users.noreply.github.com>
2025-06-19 06:16:49 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import LiveMetricsChart from './components/LiveMetricsChart';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('chart-container');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<LiveMetricsChart />);
+}
